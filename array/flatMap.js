df348c47147f8b59847c00a96934cdbc515f70fa
kalbuquerque <kalbuquerque@fornecedor.qualicorp.com.br>
2022-07-18 14:16:15 -0300

    Usando Módulos de Terceiros

--- array/flatMap.js
+++ array/flatMap.js
@@ -28,12 +28,23 @@ const escola = [
 ];
 
 const getNotaDoAluno = (aluno) => aluno.nota;
+const getNomeDoAluno = (aluno) => aluno.nome;
 const getNotasDaTurma = (turma) => turma.alunos.map(getNotaDoAluno);
+const getNomesAlunosTurma = (turma) => turma.alunos.map(getNomeDoAluno);
 
 const notas1 = escola.map(getNotasDaTurma);
 console.log(notas1);
 
-// console.log([].concat([8.1, 9.8], [8.9, 7.3]));
+console.log("");
+console.log("#### Notas dos Alunos com concat ####");
+console.log([].concat([8.1, 9.8], [8.9, 7.3]));
 
+console.log("");
+console.log("#### Notas dos Alunos com flatMap ####");
 const notas2 = escola.flatMap(getNotasDaTurma);
 console.log(notas2);
+
+console.log("");
+console.log("#### Nomes dos Alunos com flatMap ####");
+const nomesAlunos = escola.flatMap(getNomesAlunosTurma);
+console.log(nomesAlunos);
