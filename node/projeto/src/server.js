c7ffdd43d95e8b4cf2a1b9e78926f1a0006c48c0
kalbuquerque <kalbuquerque@fornecedor.qualicorp.com.br>
2022-09-01 16:35:23 -0300

    Projeto: API com Express #02

--- node/projeto/src/server.js
+++ node/projeto/src/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const dataBase = require('./data')
+
 app.get('/', (req, res) => res.send('Hello Express...'))
 
 app.get('/produtos', (req, res, next) => {
@@ -9,9 +11,16 @@ app.get('/produtos', (req, res, next) => {
   next()
 })
 
-app.get('/produtos', (req, res) => res.send([
-  { nome: 'Notebook', preco: 2399.99 },
-  { nome: 'Headset', preco: 123.45 }
-]))
+app.get('/produtos', (req, res) => res.send(dataBase.getProdutos()))
+
+app.get('/produtos/:id', (req, res) => res.send(dataBase.getProduto(req.params.id)))
+
+app.post('/produtos', (req, res, next) => {
+  const produto = dataBase.salvarProdutos({
+    nome: req.body.name,
+    preco: req.body.preco
+  })
+  res.send(produto) // JSON
+})
 
 app.listen(port, () => console.log(`Server run on port ${port}!`))
