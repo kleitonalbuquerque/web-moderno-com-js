8395dc9595c4f749a03874effe0c7a3d0f4f710c
kalbuquerque <kalbuquerque@fornecedor.qualicorp.com.br>
2022-11-04 18:32:04 -0300

    Trabalhando com múltiplos componentes

---
+++ react/exercicios/src/components/Multiplos.jsx
@@ -0,0 +1,9 @@
+export const BoaTarde = (props) => <h1>Boa tarde {props.nome}!</h1>;
+
+export const BoaNoite = (props) => <h1>Boa noite {props.nome}!</h1>;
+
+export const CoisaLinda = (props) => <h1>{props.nome} coisa linda!</h1>;
+
+export const CoisaFeia = (props) => <h1>{props.nome} coisa feia!</h1>;
+
+export default { BoaTarde, BoaNoite };
--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -2,5 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import BomDia from "./components/BomDia";
+import Multi, { CoisaLinda, CoisaFeia } from "./components/Multiplos";
 
-ReactDOM.render(<BomDia nome="Mariana" />, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <BomDia nome="Mariana" />
+    <Multi.BoaTarde nome="João" />
+    <Multi.BoaNoite nome="Maria" />
+    <CoisaLinda nome="Mariana" />
+    <CoisaFeia nome="Mulambo" />
+  </div>,
+  document.getElementById("root")
+);
